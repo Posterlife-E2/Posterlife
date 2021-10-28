864655c14bbcf39f69d40fb1969142801e6e623c
Kristoffer <kristoffer@kritix.dk>
2021-10-28 13:27:48 +0200

    Oprettet lidt logik til knap

--- app/src/main/java/com/example/posterlife/OpretBruger.kt
+++ app/src/main/java/com/example/posterlife/OpretBruger.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import com.example.posterlife.databinding.FragmentLoginBinding
+import com.example.posterlife.databinding.FragmentOpretBrugerBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +25,10 @@ class OpretBruger : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var _binding: FragmentOpretBrugerBinding? = null;
+
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -35,7 +42,17 @@ class OpretBruger : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_opret_bruger, container, false)
+        _binding = FragmentOpretBrugerBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.opretBrugerRegistrer.setOnClickListener{
+            Navigation.findNavController(it).navigate(OpretBrugerDirections.toInsFrontpage())
+        }
     }
     /**
      * @source https://stackoverflow.com/questions/29128162/android-hide-toolbar-in-specific-fragment
--- app/src/main/java/com/example/posterlife/inspirationFrontpage.kt
+++ app/src/main/java/com/example/posterlife/inspirationFrontpage.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.posterlife.databinding.FragmentInspirationFrontpageBinding
+import com.example.posterlife.databinding.FragmentOpretBrugerBinding
 
 import kotlin.jvm.JvmStatic;
 
@@ -23,6 +25,10 @@ class inspirationFrontpage : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var _binding: FragmentInspirationFrontpageBinding? = null;
+
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -36,7 +42,9 @@ class inspirationFrontpage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inspiration_frontpage, container, false)
+        _binding = FragmentInspirationFrontpageBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     companion object {
