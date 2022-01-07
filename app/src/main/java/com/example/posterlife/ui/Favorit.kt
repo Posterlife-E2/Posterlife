98c1b2a3e427611282cd7b1a844eda3f8293f5bf
Thamara Chellakooty <thamaralinnea@gmail.com>
2022-01-07 11:12:25 +0100

    Navigation between bottom bar and favorite tap Author Thamara & Camilla

--- app/src/main/java/com/example/posterlife/ui/Favorit.kt
+++ app/src/main/java/com/example/posterlife/ui/Favorit.kt
@@ -1,28 +1,36 @@
 package com.example.posterlife.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.posterlife.model.Plakat
 
 sealed class Favorit(val rute: String) {
 
-    object FavoritStart: Favorit("favoritStart")
 
+    object FavoritStart : Favorit("favoritStart")
+
+    @ExperimentalFoundationApi
     @Composable
-    fun FavoritTopBar() {
+    fun FavoritTopBar(navigation: NavHostController) {
+
+        var favoritPlakater: ArrayList<Plakat>
+
         Scaffold(
 
             topBar = {
@@ -30,19 +38,12 @@ sealed class Favorit(val rute: String) {
                     title = {
 
                         Text(
-                            text = "Inspiration",
+                            text = "Favoritter",
                             color = Color.Black,
                             fontSize = 30.sp
                         )
                     },
                     actions = {
-
-                        IconButton(onClick = { }) {
-                            Icon(
-                                Icons.Filled.Search,
-                                contentDescription = null
-                            )
-                        }
                         IconButton(onClick = { /*TODO*/ }) {
                             Icon(
                                 Icons.Filled.Favorite,
@@ -55,28 +56,40 @@ sealed class Favorit(val rute: String) {
                             Icon(Icons.Filled.ShoppingCart, contentDescription = null)
                         }
                     },
-
-
                     backgroundColor = Color(0xfffcfcf0),
-
 
                     elevation = 12.dp
                 )
             }
         ) {
-            // MainFavoritePage(navigation =  )
-            }
+            MainFavoritePage()
         }
     }
 
+    @ExperimentalFoundationApi
+    @Composable
+    fun MainFavoritePage() {
+        Column(
+            modifier = Modifier
+                .background(Color(0xfffcfcf0))
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Mine favoritter",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Light,
+                modifier = Modifier.padding(top = 5.dp)
+            )
 
-@Composable
-fun MainFavoritePage(navigation: Navigation) {
-    Column( modifier = Modifier
-        .background(Color(0xfffcfcf0))
-        .fillMaxWidth()
-        .fillMaxHeight()) {
-        Text(text = "Favoritter", fontSize = 30.sp)
+            LazyVerticalGrid(cells = GridCells.Fixed(2), contentPadding = PaddingValues(8.dp), ) {
+
+            }
+        }
+
     }
-    
+
 }
+
+
