1a8663a8e6af52427b4e8a780105308da2f1ac16
Thamara Chellakooty <thamaralinnea@gmail.com>
2022-01-10 15:20:51 +0100

    UI design finished Author Camilla og Thamara

--- app/src/main/java/com/example/posterlife/ui/Favorit.kt
+++ app/src/main/java/com/example/posterlife/ui/Favorit.kt
@@ -1,23 +1,27 @@
 package com.example.posterlife.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import coil.compose.rememberImagePainter
+import com.example.posterlife.jsonParser.PlakatInfo
 import com.example.posterlife.model.Plakat
 
 sealed class Favorit(val rute: String) {
@@ -42,7 +46,7 @@ sealed class Favorit(val rute: String) {
     }
 
     @Composable
-    fun FavoritTopBar(){
+    fun FavoritTopBar() {
         TopAppBar(
             title = {
 
@@ -75,27 +79,89 @@ sealed class Favorit(val rute: String) {
     @ExperimentalFoundationApi
     @Composable
     fun MainFavoritePage() {
+
+        val context = LocalContext.current
+        val plakatInfo = PlakatInfo(context)
+        val plakatHolder: ArrayList<Plakat> = plakatInfo.getPlakatInfo()
         Column(
-            modifier = Modifier
+            Modifier
                 .background(Color(0xfffcfcf0))
                 .fillMaxWidth()
                 .fillMaxHeight(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
             Text(
-                text = "Mine favoritter",
+                text = "Favoritter",
                 fontSize = 30.sp,
-                fontWeight = FontWeight.Light,
-                modifier = Modifier.padding(top = 5.dp)
+                fontWeight = FontWeight.Light
             )
 
-            LazyVerticalGrid(cells = GridCells.Fixed(2), contentPadding = PaddingValues(8.dp), ) {
+            LazyVerticalGrid(
+                cells = GridCells.Fixed(2),
+                contentPadding = PaddingValues(8.dp),
+            ) {
 
+                items(plakatHolder.size) { index ->
+                    Column(
+                        modifier = Modifier
+                            .padding(
+                                start = 12.dp,
+                                top = 0.dp,
+                                end = 12.dp,
+                                bottom = 12.dp
+                            )
+                    ) {
+                        IconButton(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.End).size(22.dp)) {
+                            Icon(Icons.Filled.DeleteOutline, contentDescription = null, Modifier.size(22.dp))
+
+                        }
+                        Image(
+                            painter = rememberImagePainter(
+                                data = plakatHolder.get(index).imageURL,
+                            ),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(250.dp)
+                                .clickable { }
+
+                        )
+
+                        Row(horizontalArrangement = Arrangement.End) {
+
+                            Box(modifier = Modifier.weight(1f)) {
+                            }
+                            Box(modifier = Modifier.padding(end = 4.dp)) {
+                                FavoritButton()
+                            }
+
+                            Box() {
+                                IconButton(onClick = { /*TODO*/ }, modifier = Modifier.size(22.dp)) {
+                                    Icon(Icons.Filled.ShoppingCart, contentDescription = null, Modifier.size(22.dp))
+                                }
+                            }
+                            }
+                        }
+                    }
+                }
             }
+
         }
-
     }
-
-}
-
+    @Composable
+    fun FavoritButton(
+        color: Color = Color.Red,
+    ) {
+        var isFavorite by remember { mutableStateOf(false) }
+        IconToggleButton(checked = isFavorite, modifier = Modifier.size(22.dp), onCheckedChange = { isFavorite = !isFavorite }) {
+            Icon(
+                tint = color, modifier = Modifier.size(22.dp), imageVector = if (isFavorite) {
+                    Icons.Filled.Favorite
+                } else {
+                    Icons.Default.FavoriteBorder
+                },
+                contentDescription = null
+            )
+        }
+    }
 
