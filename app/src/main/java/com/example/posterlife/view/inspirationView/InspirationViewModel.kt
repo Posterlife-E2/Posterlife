78f6207d9a26d9f7408d41d601c26dab4cc192f6
Kristoffer <kristoffer@kritix.dk>
2022-01-12 15:09:40 +0100

    viewmodel implemented

--- app/src/main/java/com/example/posterlife/view/inspirationView/InspirationViewModel.kt
+++ app/src/main/java/com/example/posterlife/view/inspirationView/InspirationViewModel.kt
@@ -10,6 +10,14 @@ import androidx.lifecycle.ViewModel
  *
  * @Source https://developer.android.com/jetpack/compose/state#viewmodel-and-jetpack-compose
  * @Source https://proandroiddev.com/jetpack-compose-navigation-architecture-with-viewmodels-1de467f19e1c
+ *
+ * Det er med fuld belæg at dette er et objekt. Compose ser gerne at vi undgår at pass arguments mellem sider, så vidt muligt
+ * Derfor giver det god mening at lave vores viewModel til et objekt, hvis selve data staten er irrelevant over flere screens.
+ * Vi skal kun bruge en plakatIndex: Int kortvarigt.
+ *
+ * Denne viewModel har kun til opgave at pass currentIndex til vores FocusImage screen, som giver mere information om en
+ * given plakat fra inspiration.
+ *
  */
 
 object InspirationViewModel : ViewModel() {
